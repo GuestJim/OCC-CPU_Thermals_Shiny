5a905a136904d3bc3eeee2ff16d6c4ba4cdba5ad
GuestJim <30958420+GuestJim@users.noreply.github.com>
2022-09-09 23:50:38 -0400

    Cleaner means of passing info
    Found a cleaner means of passing information into modules and am applying it here

--- app_tables_multi.r
+++ app_tables_multi.r
@@ -1,16 +1,17 @@
-tableCUSTServer	<-	function(name, IN, TYPE, roundTerm, OFF = 0, UNIT = NULL)	{	moduleServer(name, function(input, output, session)	{
+tableCUSTServer	<-	function(name, TYPE, OFF = 0, UNIT = NULL)	{
+	IN	<-	DATA$dataALL	;	roundTerm	<- reactive(input$roundTerm)	;	moduleServer(name, function(input, output, session)	{
 	if (!is.null(UNIT))	updateTextInput(inputId = "ECDF",	label = paste0("Specific Values (", UNIT, ")"))
 
 	PERCs	<-	reactive(	tablePERC(IN, TYPE, to.NUM(input$PERC), UNIT, OFF)	)	%>%	bindCache(IN, input$PERC)
 	ECDFs	<-	reactive(	tableECDF(IN, TYPE, to.NUM(input$ECDF), UNIT, OFF)	)	%>%	bindCache(IN, input$ECDF)
 
-	output$PERCtab	<-	renderTable(	PERCs(),	striped = TRUE,	digits = reactive(roundTerm)	)
-	output$ECDFtab	<-	renderTable(	ECDFs(),	striped = TRUE,	digits = reactive(roundTerm)	)
+	output$PERCtab	<-	renderTable(	PERCs(),	striped = TRUE,	digits = reactive(roundTerm())	)
+	output$ECDFtab	<-	renderTable(	ECDFs(),	striped = TRUE,	digits = reactive(roundTerm())	)
 })}
 
-observeEvent(list(input$dataSelLOAD, input$roundTerm, input$engPOW),	{
-	tableCUSTServer("TEMP",	DATA$dataALL,	"CPU_Temp",			input$roundTerm,	OFF = TABLE$warmOFF())
-	tableCUSTServer("FREQ",	DATA$dataALL,	"Frequency",		input$roundTerm)
-	tableCUSTServer("SOCK",	DATA$dataALL,	"Socket_Energy",	input$roundTerm,	UNIT = input$engPOW)
-	tableCUSTServer("CORE",	DATA$dataALL,	"Core_Energy",		input$roundTerm,	UNIT = input$engPOW)
+observeEvent(list(input$dataSelLOAD, input$engPOW),	{
+	tableCUSTServer("TEMP",	"CPU_Temp",			OFF = TABLE$warmOFF())
+	tableCUSTServer("FREQ",	"Frequency")
+	tableCUSTServer("SOCK",	"Socket_Energy",	UNIT = input$engPOW)
+	tableCUSTServer("CORE",	"Core_Energy",		UNIT = input$engPOW)
 })
